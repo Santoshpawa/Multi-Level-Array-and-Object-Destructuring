0cf32927deb176fdce14ec007a844c96057330db
Santosh Singh Pawar <textingsantosh@gmail.com>
2025-04-03 10:54:50 +0530

    program made

---
+++ program.js
@@ -0,0 +1,13 @@
+const people = [ { name: "Alice", 
+                   address: { city: "New York", 
+                              street: { name: "Broadway", 
+                                        number: 123 } } }, 
+                 { name: "Bob", 
+                   address: { city: "Los Angeles", 
+                   street: { name: "Sunset Boulevard", 
+                             number: 456 } } } ];
+
+let result = people.map(({name:personName, 
+        address:{city, street:{name:streetName}}})=> `${personName} lives in ${city} on ${streetName}`)
+ 
+console.log(result)
